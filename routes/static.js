7c2dc35fe39614e30396b80d849bf5c505d7f6a2
D-4costa <danielleelisabeth01@gmail.com>
2026-01-10 14:21:12 -0500

    Add routes for custom, sedan, SUV, and truck

--- routes/static.js
+++ routes/static.js
@@ -2,11 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", {
-    title: "Home | CSE Motors"
-  });
+  res.render("index", { title: "Home | CSE Motors" });
+});
+
+router.get("/custom", (req, res) => {
+  res.render("custom", { title: "Custom | CSE Motors" });
+});
+
+router.get("/sedan", (req, res) => {
+  res.render("sedan", { title: "Sedan | CSE Motors" });
+});
+
+router.get("/suv", (req, res) => {
+  res.render("suv", { title: "SUV | CSE Motors" });
+});
+
+router.get("/truck", (req, res) => {
+  res.render("truck", { title: "Truck | CSE Motors" });
 });
 
 module.exports = router;
-
-
