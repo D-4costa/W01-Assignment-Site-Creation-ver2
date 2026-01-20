29151058710429b2936c0b4521d63883ea3a5b74
D-4costa <danielleelisabeth01@gmail.com>
2026-01-19 19:30:39 -0500

    Create index.js

---
+++ utilities/index.js
@@ -0,0 +1,38 @@
+const Util = {}
+const jwt = require("jsonwebtoken")
+
+Util.handleErrors = (fn) => {
+  return function (req, res, next) {
+    Promise.resolve(fn(req, res, next)).catch(next)
+  }
+}
+
+Util.buildVehicleDetail = function (vehicle) {
+  const price = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(vehicle.inv_price)
+
+  const mileage = new Intl.NumberFormat("en-US").format(vehicle.inv_miles)
+
+  return `
+  <section class="vehicle-detail">
+    <div class="vehicle-image">
+      <img 
+        src="${vehicle.inv_image}" 
+        alt="Image of ${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}"
+      >
+    </div>
+
+    <div class="vehicle-info">
+      <h2>${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}</h2>
+      <p><strong>Price:</strong> ${price}</p>
+      <p><strong>Mileage:</strong> ${mileage} miles</p>
+      <p><strong>Description:</strong> ${vehicle.inv_description}</p>
+      <p><strong>Color:</strong> ${vehicle.inv_color}</p>
+    </div>
+  </section>
+  `
+}
+
+module.exports = Util
