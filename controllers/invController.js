2c4a35ff69eb496e8dbcae6b60d64f69c384dc1f
D-4costa <danielleelisabeth01@gmail.com>
2026-01-22 20:37:29 -0500

    Update invController.js

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,29 +1,46 @@
-const path = require("path")
-
-const invModel = require(
-  path.join(process.cwd(), "models/inventory-model")
-)
-const utilities = require(
-  path.join(process.cwd(), "utilities")
-)
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities")
 
 const invCont = {}
 
-invCont.buildInventoryDetail = async function (req, res, next) {
-  const inv_id = req.params.inv_id
-  const vehicle = await invModel.getInventoryById(inv_id)
+/* ***************************
+ * Build inventory by classification
+ * ************************** */
+invCont.buildByClassificationId = async function (req, res) {
+  const classification_id = req.params.classificationId
+  const data = await invModel.getInventoryByClassificationId(classification_id)
 
-  if (!vehicle) {
-    const error = new Error("Vehicle not found")
-    error.status = 404
-    throw error
+  if (!data || data.length === 0) {
+    throw new Error("No vehicles found for this classification.")
   }
 
-  const vehicleHTML = utilities.buildVehicleDetail(vehicle)
+  const grid = await utilities.buildClassificationGrid(data)
+  const nav = await utilities.getNav()
+  const className = data[0].classification_name
+
+  res.render("./inventory/classification", {
+    title: `${className} vehicles`,
+    nav,
+    grid,
+  })
+}
+
+/* ***************************
+ * Build inventory detail view
+ * ************************** */
+invCont.buildInventoryDetail = async function (req, res) {
+  const inv_id = req.params.inv_id
+  const vehicleData = await invModel.getInventoryById(inv_id)
+
+  if (!vehicleData || vehicleData.length === 0) {
+    throw new Error("Vehicle not found.")
+  }
+
+  const vehicleHTML = await utilities.buildVehicleDetail(vehicleData[0])
   const nav = await utilities.getNav()
 
-  res.render("inventory/detail", {
-    title: `${vehicle.inv_make} ${vehicle.inv_model}`,
+  res.render("./inventory/detail", {
+    title: `${vehicleData[0].inv_make} ${vehicleData[0].inv_model}`,
     nav,
     vehicleHTML,
   })
