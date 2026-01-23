2d832a176bc1f4582052075c8a9387041a1f080e
D-4costa <danielleelisabeth01@gmail.com>
2026-01-22 20:14:58 -0500

    Update inventory-model.js

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,9 +1,14 @@
-const pool = require("../database/")
+const path = require("path")
+
+const pool = require(
+  path.join(process.cwd(), "database")
+)
 
 async function getInventoryById(inv_id) {
   try {
     const sql = `
-      SELECT * FROM inventory
+      SELECT *
+      FROM inventory
       WHERE inv_id = $1
     `
     const data = await pool.query(sql, [inv_id])
