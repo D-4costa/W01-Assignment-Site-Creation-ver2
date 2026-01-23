06e48e6724b5562d66a475274ea26ed6225a8ce1
D-4costa <danielleelisabeth01@gmail.com>
2026-01-22 20:55:07 -0500

    Update inventory-model.js

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,28 +1,36 @@
 const pool = require("../database")
 
 /* ***************************
- * Get inventory by classification
+ * Get inventory by classification ID
  * ************************** */
 async function getInventoryByClassificationId(classification_id) {
-  const data = await pool.query(
-    `SELECT * FROM public.inventory AS i
-     JOIN public.classification AS c
-     ON i.classification_id = c.classification_id
-     WHERE i.classification_id = $1`,
-    [classification_id]
-  )
-  return data.rows
+  try {
+    const sql = `
+      SELECT * FROM public.inventory AS i
+      JOIN public.classification AS c
+      ON i.classification_id = c.classification_id
+      WHERE i.classification_id = $1
+    `
+    const data = await pool.query(sql, [classification_id])
+    return data.rows
+  } catch (error) {
+    console.error("getInventoryByClassificationId error:", error)
+    throw error
+  }
 }
 
 /* ***************************
- * Get inventory item by ID
+ * Get inventory item by inventory ID
  * ************************** */
 async function getInventoryById(inv_id) {
-  const data = await pool.query(
-    "SELECT * FROM public.inventory WHERE inv_id = $1",
-    [inv_id]
-  )
-  return data.rows
+  try {
+    const sql = "SELECT * FROM public.inventory WHERE inv_id = $1"
+    const data = await pool.query(sql, [inv_id])
+    return data.rows
+  } catch (error) {
+    console.error("getInventoryById error:", error)
+    throw error
+  }
 }
 
 module.exports = {
