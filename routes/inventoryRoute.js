8e9aeaf4899d6354e3fb8d61d28b47bba72a57a9
D-4costa <danielleelisabeth01@gmail.com>
2026-01-22 20:53:57 -0500

    Update inventoryRoute.js

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,15 +1,19 @@
 const express = require("express")
-const router = new express.Router()
+const router = express.Router()
 const invController = require("../controllers/invController")
 const utilities = require("../utilities")
 
-// Inventory by classification
+/* ***************************
+ * Inventory by classification
+ * ************************** */
 router.get(
   "/type/:classificationId",
   utilities.handleErrors(invController.buildByClassificationId)
 )
 
-// Inventory detail
+/* ***************************
+ * Inventory detail
+ * ************************** */
 router.get(
   "/detail/:inv_id",
   utilities.handleErrors(invController.buildInventoryDetail)
