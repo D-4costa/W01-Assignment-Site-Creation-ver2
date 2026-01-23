5ce16c735e7e15d85c300e73b32cf1bd00f8495a
D-4costa <danielleelisabeth01@gmail.com>
2026-01-22 20:51:42 -0500

    Update invController.js

--- controllers/invController.js
+++ controllers/invController.js
@@ -6,44 +6,56 @@ const invCont = {}
 /* ***************************
  * Build inventory by classification
  * ************************** */
-invCont.buildByClassificationId = async function (req, res) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
+invCont.buildByClassificationId = async function (req, res, next) {
+  try {
+    const classification_id = req.params.classificationId
+    const data = await invModel.getInventoryByClassificationId(classification_id)
 
-  if (!data || data.length === 0) {
-    throw new Error("No vehicles found for this classification.")
+    if (!data || data.length === 0) {
+      const error = new Error("No vehicles found for this classification.")
+      error.status = 404
+      throw error
+    }
+
+    const grid = await utilities.buildClassificationGrid(data)
+    const nav = await utilities.getNav()
+    const className = data[0].classification_name
+
+    res.render("inventory/classification", {
+      title: `${className} vehicles`,
+      nav,
+      grid,
+    })
+  } catch (error) {
+    next(error)
   }
-
-  const grid = await utilities.buildClassificationGrid(data)
-  const nav = await utilities.getNav()
-  const className = data[0].classification_name
-
-  res.render("./inventory/classification", {
-    title: `${className} vehicles`,
-    nav,
-    grid,
-  })
 }
 
 /* ***************************
  * Build inventory detail view
  * ************************** */
-invCont.buildInventoryDetail = async function (req, res) {
-  const inv_id = req.params.inv_id
-  const vehicleData = await invModel.getInventoryById(inv_id)
+invCont.buildInventoryDetail = async function (req, res, next) {
+  try {
+    const inv_id = req.params.inv_id
+    const data = await invModel.getInventoryById(inv_id)
 
-  if (!vehicleData || vehicleData.length === 0) {
-    throw new Error("Vehicle not found.")
+    if (!data || data.length === 0) {
+      const error = new Error("Vehicle not found.")
+      error.status = 404
+      throw error
+    }
+
+    const vehicleHTML = utilities.buildVehicleDetail(data[0])
+    const nav = await utilities.getNav()
+
+    res.render("inventory/detail", {
+      title: `${data[0].inv_make} ${data[0].inv_model}`,
+      nav,
+      content: vehicleHTML,
+    })
+  } catch (error) {
+    next(error)
   }
-
-  const vehicleHTML = await utilities.buildVehicleDetail(vehicleData[0])
-  const nav = await utilities.getNav()
-
-  res.render("./inventory/detail", {
-    title: `${vehicleData[0].inv_make} ${vehicleData[0].inv_model}`,
-    nav,
-    vehicleHTML,
-  })
 }
 
 module.exports = invCont
