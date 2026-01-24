d4d0a9d772e4adeb4ff530b86b625a69130657b2
D-4costa <danielleacosta01@gmail.com>
2026-01-24 01:23:12 -0500

    fix

--- utilities/index.js
+++ utilities/index.js
@@ -1,57 +1,80 @@
-const Util = {}
-const invModel = require("../models/inventory-model")
+const Util = {};
+const invModel = require("../models/inventory-model");
 
-Util.handleErrors = fn => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next)
+Util.getNav = async function () {
+  try {
+    const data = await invModel.getClassifications();
+    let list = "<ul>";
+    list += '<li><a href="/" title="Home page">Home</a></li>';
+    data.rows.forEach((row) => {
+      list += "<li>";
+      list +=
+        '<a href="/inv/type/' +
+        row.classification_id +
+        '" title="See our inventory of ' +
+        row.classification_name +
+        ' vehicles">' +
+        row.classification_name +
+        "</a>";
+      list += "</li>";
+    });
+    list += "</ul>";
+    return list;
+  } catch (error) {
+    // Fallback nav when database is unavailable
+    return (
+      '<ul>' +
+      '<li><a href="/" title="Home page">Home</a></li>' +
+      "</ul>"
+    );
+  }
+};
+
+Util.handleErrors = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
 
 /* ***************************
  * Build classification grid
  * ************************** */
 Util.buildClassificationGrid = async function (data) {
-  let grid = '<ul id="inv-display">'
+  let grid = '<ul id="inv-display" aria-label="Inventory results">';
 
-  data.forEach(vehicle => {
+  data.forEach((vehicle) => {
+    const price = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(vehicle.inv_price);
+
     grid += `
-      <li>
-        <a href="/inv/detail/${vehicle.inv_id}">
-          <img src="${vehicle.inv_thumbnail}"
-               alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}">
+      <li class="inv-card">
+        <a class="inv-card_link" href="/inv/detail/${vehicle.inv_id}" aria-label="View ${vehicle.inv_make} ${vehicle.inv_model} details">
+          <img class="inv-card_img" src="${vehicle.inv_thumbnail}" alt="${vehicle.inv_make} ${vehicle.inv_model}">
+          <div class="inv-card_overlay">
+            <h3 class="inv-card_title">${vehicle.inv_make} ${vehicle.inv_model}</h3>
+            <p class="inv-card_price">${price}</p>
+          </div>
         </a>
-        <div class="namePrice">
-          <hr>
-          <h2>
-            <a href="/inv/detail/${vehicle.inv_id}">
-              ${vehicle.inv_make} ${vehicle.inv_model}
-            </a>
-          </h2>
-          <span>
-            ${new Intl.NumberFormat("en-US", {
-              style: "currency",
-              currency: "USD",
-            }).format(vehicle.inv_price)}
-          </span>
-        </div>
-      </li>`
-  })
+      </li>`;
+  });
 
-  grid += "</ul>"
-  return grid
-}
+  grid += "</ul>";
+  return grid;
+};
 
 /* ***************************
  * Build vehicle detail
  * ************************** */
 Util.buildVehicleDetail = function (vehicle) {
   if (!vehicle) {
-    return "<p>Vehicle information not available.</p>"
+    return "<p>Vehicle information not available.</p>";
   }
 
   const price = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-  }).format(vehicle.inv_price)
+  }).format(vehicle.inv_price);
 
-  const miles = new Intl.NumberFormat("en-US").format(vehicle.inv_miles)
+  const miles = new Intl.NumberFormat("en-US").format(vehicle.inv_miles);
 
   return `
     <section class="vehicle-detail">
@@ -68,10 +91,35 @@ Util.buildVehicleDetail = function (vehicle) {
         <p><strong>Color:</strong> ${vehicle.inv_color}</p>
       </div>
     </section>
-  `
-}
+  `;
+};
 
-module.exports = Util
+/**
+ * Build an HTML select element with classification data
+ * @param {int} classification_id
+ * @returns {string}
+ */
 
+Util.buildClassificationList = async function (classification_id = null) {
+  let data = await invModel.getClassifications();
+  let classificationList =
+    '<select name="classification_id" id="classificationList" required>';
+  classificationList += "<option value=''>Choose a Classification</option>";
+  data.rows.forEach((row) => {
+    classificationList += '<option value="' + row.classification_id + '"';
+    if (
+      classification_id != null &&
+      row.classification_id == classification_id
+    ) {
+      classificationList += " selected ";
+    }
+    classificationList += ">" + row.classification_name + "</option>";
+  });
+  classificationList += "</select>";
+  return classificationList;
+};
 
+Util.handleErrors = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
 
+module.exports = Util;
